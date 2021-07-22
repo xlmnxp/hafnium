e9af1b594f6f6e60ba6f5d2bfeeaf5c0c8a47004
Salem Yaslem <s@sy.sa>
2021-07-22 12:06:17 +0300

    use io::copy to forward connect, use logger, make utils and move forward_duplex_stream to it

--- target/rls/debug/deps/hf-d8803e1982641a59.d
+++
@@ -1,5 +0,0 @@
-/home/syaslem/Projects/hafnium/target/rls/debug/deps/hf-d8803e1982641a59.rmeta: src/main.rs
-
-/home/syaslem/Projects/hafnium/target/rls/debug/deps/hf-d8803e1982641a59.d: src/main.rs
-
-src/main.rs:
